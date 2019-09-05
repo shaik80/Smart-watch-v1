6b81db49a7ece541c1907d08ed00a9001cbe28ce
shaik80 <sh.mudassir98@gmail.com>
2019-09-05 18:56:13 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $(function(){
     $( ".display-border" ).hide();
+    // $( ".message-notification" ).hide();
+    $( ".homescreen" ).hide();
     $( ".round-icon" ).click(function() {
         $( ".display-border" ).show();
+        $(".homescreen").hide();
         $( ".watch-screen" ).css({'background-color':'#222831','background-image':'none'});
+
     })
 
 })
-function hidecover(){
-    
-}
