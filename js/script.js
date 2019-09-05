75c9cc68900c4d0b0969318373e5ef88d2fc0855
shaik80 <sh.mudassir98@gmail.com>
2019-09-05 17:35:44 +0530

    change

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-// function call(){
-//     alert("hello");
-// }
+$(function(){
+    $( ".display-border" ).hide();
+    $( ".round-icon" ).click(function() {
+        $( ".display-border" ).show();
+        $( ".watch-screen" ).css({'background-color':'#222831','background-image':'none'});
+    })
+
+})
+function hidecover(){
+    
+}
